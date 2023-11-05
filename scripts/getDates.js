bba86397d60af3b1674ce443bf3f126cf9762a45
jase863 <jason.walker8630@gmail.com>
2023-11-05 01:20:36 -0600

    final changes, checked html and css for errors

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,9 @@
 const date = new Date();
 
-document.querySelector("p").innerHTML = `&copy;${date.getFullYear()}<br>Jason Walker<br>Utah`
+if (visualViewport.width < 640) {
+  
+    document.querySelector("p").innerHTML = `&copy;${date.getFullYear()}<br>Jason Walker<br>Utah<br>`
+
+} else {document.querySelector("p").innerHTML = `&copy;${date.getFullYear()} | Jason Walker | Utah`};
 
 document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;

394efad5496cd7e2dc1b92ae5ea5273e03dc23f9
jase863 <jason.walker8630@gmail.com>
2023-11-14 23:26:49 -0700

    finished lazy loading practice

--- week03/scripts/getDates.js
+++ week03/scripts/getDates.js
@@ -1,9 +1,5 @@
 const date = new Date();
 
-if (visualViewport.width < 640) {
-  
-    document.querySelector("p").innerHTML = `&copy;${date.getFullYear()}<br>Jason Walker<br>Utah<br>`
-
-} else {document.querySelector("p").innerHTML = `&copy;${date.getFullYear()} | Jason Walker | Utah`};
+document.getElementById("year").innerHTML = `&copy;${date.getFullYear()}`;
 
 document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;

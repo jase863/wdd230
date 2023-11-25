7b8fe5b1afcd3e1c3ec0c91f5ba180974f3155ca
jase863 <jason.walker8630@gmail.com>
2023-11-24 22:58:06 -0700

    fixed date issues

--- chamber/scripts/lengthSinceVisit.js
+++ chamber/scripts/lengthSinceVisit.js
@@ -1,26 +1,33 @@
 let message = document.getElementById("message");
 
-const msConversion = 84600000;
+const msConversion = 86400000 ;
 const dateToday = new Date();
 
 const today = Date.now();
 
 let lastVisit = Number(window.localStorage.getItem("daysSinceVisit")) || 0;
 
-let daysPassed = (lastVisit - today) / msConversion;
+let daysPassed = (today/msConversion)-(lastVisit/msConversion);
 
 if (daysPassed !== 0){
 
     if (daysPassed < 1 > 0){
 
         message.innerHTML = "Back so soon! Awesome!";
-    } else if (daysPassed > 1){
-        message.innerHTML = `You last visted ${daysPassed.toFixed(0)} days ago.`;
+    } else if (daysPassed >= 1){
+        if (daysPassed == 1){
+
+        message.innerHTML = `You last visted ${daysPassed.toFixed(0)} day ago.`;
+
+        }else{
+           
+            message.innerHTML = `You last visted ${daysPassed.toFixed(0)} days ago.`;
+
+        }
+
     }
 } else{
     message.innerHTML = "Welcome! Let us know if you have any questions!";
 }
 
-localStorage.setItem("daysSinceVisit", daysPassed);
-
-alert(daysPassed);
+localStorage.setItem("daysSinceVisit", today);

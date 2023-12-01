8bead913e59edf05acb3768eaa6c2f08c20ce2dc
jase863 <jason.walker8630@gmail.com>
2023-12-01 11:51:13 -0700

    fixed links javascript

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,7 @@
 const baseURL = "https://jase863.github.io/wdd230/";
 const linksURL = "https://jase863.github.io/wdd230/data/links.json";
-const activities = document.querySelector('#activities');
+
+const activities = document.getElementById('activities');
 
 async function getLinks() {
 
@@ -8,38 +9,42 @@ async function getLinks() {
 
     const data = await response.json();
 
-    console.log(data);
+    console.table(data.weeks);
 
-    displayLinks(data);
+    displayLinks(data.weeks);
     
 }
 
 getLinks();
 
 function displayLinks(weeks) {
-    weeks.array.forEach((week) => {
-        let card = document.createElement("section");
-        let fullName = document.createElement("h2");
-        let portrait = document.createElement("img");
-        let birthDate = document.createElement("h3");
-        let birthPlace = document.createElement("h3");
+    weeks.forEach((week) => {
+        let weeksList = document.createElement("li");
+        let weekNum = week.week;
+        let links = week.links
 
-        fullName.textContent = `${week.week} ${week.week.url}`;
-        birthDate.textContent = `Date of Birth ${prophet.birthdate}`
-        birthPlace.textContent = `Place of Birth ${prophet.birthplace}`
+        weeksList.textContent = `${weekNum}: `;
 
-        portrait.setAttribute("src", prophet.imageurl);
-        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute("loading", "lazy");
-        portrait.setAttribute("width", "340");
-        portrait.setAttribute("height", "440");
+        links.forEach((link) =>{
 
-        card.appendChild(fullName);
-        card.appendChild(birthDate);
-        card.appendChild(birthPlace);
-        card.appendChild(portrait);
+            let linkAnchor = document.createElement("a");
+            let linkRef = link.url;
+            let linkTitle = link.title;
 
+            linkAnchor.href = (linkRef);
 
-        cards.appendChild(card);
+            if (link != links[links.length - 1]){
+
+                linkAnchor.innerHTML = `${linkTitle} | `;
+
+            } else{
+                linkAnchor.innerHTML = `${linkTitle}`
+            }
+            
+
+            weeksList.appendChild(linkAnchor);
+        })
+
+        activities.appendChild(weeksList);
     });
 }
